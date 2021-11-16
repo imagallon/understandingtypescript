fa922b8990535b37d7929119cd38b1f78fee0497
Izzy <browncans@gmail.com>
2021-11-16 14:42:20 -0800

    more

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = 'izzy';
-if (typeof userInput === 'string') {
-    userName = userInput;
-}
-;
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-}
-generateError('An error occured!', 500);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Department = /** @class */ (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        // private readonly id: string;
+        // private name: string;
+        this.employees = [];
+        // this.id = id;
+        // this.name = n;
+    }
+    Department.prototype.describe = function () {
+        console.log("Department (" + this.id + "): " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        // validation etc
+        // this.id = 'd2';
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var ITDepartment = /** @class */ (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var accounting = new Department('d1', 'Accounting');
+var it = new ITDepartment('d2', ['Israel']);
+it.addEmployee('Jimmy');
+it.addEmployee('Manuel');
+// it.employees[2] = 'Anna';
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+// accounting.employees[2] = 'Anna';
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// accountingCopy.describe();
--- app.ts
+++ app.ts
@@ -1,14 +1,59 @@
-let userInput: unknown;
-let userName: string;
+class Department {
+  // private readonly id: string;
+  // private name: string;
+  private employees: string[] = [];
 
-userInput = 5;
-userInput = 'izzy';
-if (typeof userInput === 'string') {
-  userName = userInput
-};
+  constructor(private readonly id: string, public name: string) {
+    // this.id = id;
+    // this.name = n;
+  }
 
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    // validation etc
+    // this.id = 'd2';
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
-generateError('An error occured!', 500);
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+
+  }
+}
+
+const accounting = new Department('d1', 'Accounting');
+const it = new ITDepartment('d2', ['Israel']);
+
+it.addEmployee('Jimmy');
+it.addEmployee('Manuel');
+
+// it.employees[2] = 'Anna';
+
+it.describe();
+it.name = 'NEW NAME';
+it.printEmployeeInformation();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';
+
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
